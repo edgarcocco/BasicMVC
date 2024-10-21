d066022a7b185025520edf4387eb56d0e879221c
Edgar V. Cocco <edgar34_@hotmail.com>
2024-10-21 19:27:58 -0400

    Moved project to src folder

---
+++ src/DataLayer/BasicDBContext.cs
@@ -0,0 +1,13 @@
+﻿using BasicMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicMVC.DataLayer
+{
+    public class BasicDBContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; } = null!;
+        public BasicDBContext(DbContextOptions<BasicDBContext> options) : base(options)
+        {
+        }
+    }
+}
